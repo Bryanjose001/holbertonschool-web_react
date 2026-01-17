5b14b83aac0ab69c3951d991262eb12a5157ba73
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-17 15:55:59 -0400

    task0

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student{
+    firstName: string
+    lastName: string
+    age: number
+    location: string
+}
+
+const student1:Student = {
+    firstName:'Bryan',
+    lastName:'Jose',
+    age:23,
+    location:'PuertoRico'
+}
+const student2:Student = {
+    firstName:'Jose',
+    lastName:'Yan',
+    age:24,
+    location:'PuertoRico'
+}
+const studentslist:Student[] =[student1,student2]
