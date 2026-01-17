5ef856a5c69993f4b9fc2e6236c1cc4731439b11
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-17 16:40:10 -0400

    typescript

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,50 @@
+interface Teacher{
+    readonly firstName:string
+    readonly lastName: string
+    fullTimeEmployee: boolean
+    yearsOfExperience?: number
+    location: string
+    [key: string]:any
+}
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher{
+    numberOfReports:number
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+function printTeacher(firstName:string,lastName:string):string{
+    return `${firstName[0]}. ${lastName}`
+}
+interface printTeacherFunction{
+    (firstName:string,lastName:string):string
+}
+console.log(printTeacher('Bryan','Jose'))
+
+class StudentClass{
+    constructor(firstName:string,lastName:string){}
+    workOnHomework():string{
+        return 'Currently working'
+    }
+    displayName():string{
+        return this.firstName
+    }
+    
+}
