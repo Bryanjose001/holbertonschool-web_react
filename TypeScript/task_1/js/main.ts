5feaa47561ac83a7b4257cc19c08a3d795966a33
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-18 01:16:37 -0400

    typescript the whole proyect

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -38,13 +38,38 @@ interface printTeacherFunction{
 }
 console.log(printTeacher('Bryan','Jose'))
 
-class StudentClass{
-    constructor(firstName:string,lastName:string){}
-    workOnHomework():string{
-        return 'Currently working'
-    }
-    displayName():string{
-        return this.firstName
-    }
-    
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
 }
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const Student: StudentConstructor = StudentClass;
+
+
+const student1 = new Student('Bryan', 'Camacho');
+
+console.log(student1.displayName());
+console.log(student1.workOnHomework());
